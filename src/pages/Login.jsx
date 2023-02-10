537e951fa40d8b9a0c76b44176b8b9c56f652303
benshabbat <benshabbat27@gmail.com>
2023-02-10 14:05:07 +0200

    start page login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 const Login = () => {
+  const [formData, setFormData] = useState()
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const onSubmit = (e) => e.preventDefault();
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Put your User name and Password</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              placeholder="UserName for the user"
+              className="form-control"
+              type="text"
+              name="username"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              placeholder="Password for the user"
+              className="form-control"
+              type="password"
+              name="password"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
 
 export default Login
