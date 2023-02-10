a367edbd3a7004fe1ccb4e68ed24e41ff5542618
benshabbat <benshabbat27@gmail.com>
2023-02-10 11:28:12 +0200

    add react icons create header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Register from "./pages/Register";  
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Dashboard/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
-    </Routes>
-  </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {FaSignInAlt,faSignOutAlt,FaUser} from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="logo">
+            <Link to="/">Garage770</Link> 
+        </div>
+        <ul>
+            <li>
+                <Link to="/login"><FaSignInAlt/>Login</Link>
+            </li>
+            <li>
+                <Link to="/register"><FaUser/>Register</Link>
+            </li>
+        </ul>
+    </div>
   )
 }
 
