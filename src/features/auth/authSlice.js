aaadafca0a13aa5bcfd1f65890cadc147491ca6e
benshabbat <benshabbat27@gmail.com>
2023-02-13 16:29:50 +0200

    update store and auth

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,25 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+//Get User from localStorage
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+//Create new User
+
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, token: null },
+  initialState,
   reducers: {
-    setCredentials: (state, action) => {
-      console.log(action.payload)
-      const { user, access_token } = action.payload;
-      state.user = user;
-      state.token = access_token;
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
     },
-    logOut: (state, action) => {
-      state.user = null;
-      state.token = null;
-    },
+    // setCredentials: (state, action) => {
+    //   console.log(action.payload);
+    //   const { user, access_token } = action.payload;
+    //   state.user = user;
+    //   state.token = access_token;
+    // },
+    // logOut: (state, action) => {
+    //   state.user = null;
+    //   state.token = null;
+    // },
   },
+  extraReducers: () => {},
 });
 
-export const { setCredentials, logOut } = authSlice.actions;
+export const { reset, setCredentials, logOut } = authSlice.actions;
 
 export default authSlice.reducer;
 
-export const selectCurrentUser = (state) => state.auth.user;
-export const selectCurrentToken = (state) => state.auth.token;
+// export const selectCurrentUser = (state) => state.auth.user;
+// export const selectCurrentToken = (state) => state.auth.token;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import App from "./App";
 //import { SearchContextProvider } from "./context/SearchContext";
 //import { AuthContextProvider } from "./context/AuthContext";
 //import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { store } from "./app/store";
-// import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
