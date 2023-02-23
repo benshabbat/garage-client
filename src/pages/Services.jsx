83bc1cb91a8fa95f1cb32acc497adc6e018dd007
benshabbat <benshabbat27@gmail.com>
2023-02-23 14:43:43 +0200

    add page services

---
+++ src/pages/Services.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getServicesByIdCar } from "../features/user/userSlice";
+import { useParams } from "react-router-dom";
+const Services = () => {
+  const dispatch = useDispatch();
+
+  // const { user } = useSelector((state) => state.user);
+  const { services, user } = useSelector((state) => state.user);
+debugger
+  const { carId } = useParams();
+
+  // const { cars } = user;
+  
+
+  const carNumberPlate = user.cars.filter(car=>carId === car._id)[0].numberPlate
+  // const carNumberPlate = car.map(car=>car.numberPlate)
+  // debugger
+
+  useEffect(() => {
+    dispatch(getServicesByIdCar(carId));
+  }, [carId, dispatch]);
+
+  //  useEffect(() => dispatch(getServicesByIdCar(carId)), [carId, dispatch]);
+  return (
+    <>
+      {/* <h1>{`hello ${carId}`}</h1> */}
+      <h2>{`histo Services for ${carNumberPlate} `}</h2>
+
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {services &&
+            services.map((service) => {
+              return (
+                <tr key={service._id}>
+                  <td>{service.title}</td>
+                  <td>{service.description}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Services;
