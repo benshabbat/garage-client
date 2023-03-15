8cddc13370a4d8a105c7f18ae31b1396034174c0
benshabbat <benshabbat27@gmail.com>
2023-03-15 21:25:01 +0200

    start ui reviews

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./reviews.css";
 
 const Reviews = () => {
-  return (
-    <div id="reviews" style={{height:500}}>Reviews</div>
-  )
-}
+  const reviewsCustomers = [
+    {
+      name: "tal",
+      desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,",
+      stars: 4,
+    },
+    {
+      name: "yossi",
+      desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,",
+      stars: 3,
+    },
+    {
+      name: "dana",
+      desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. ",
+      stars: 5,
+    },
+    {
+      name: "moshe",
+      desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text.",
+      stars: 4,
+    },
+  ];
 
-export default Reviews
+  return (
+    <div id="reviews" className="reviews">
+      <div className="reviews-container">
+        {reviewsCustomers.map((customer) => {
+          return (
+            <div className="one-review">
+              <h1>{customer.name}</h1>
+              <div className="stars">
+                <div className="star" />
+                <div className="star" />
+                <div className="star" />
+              </div>
+              <div className="desc">{customer.desc}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="create-review"></div>
+    </div>
+  );
+};
+
+export default Reviews;
