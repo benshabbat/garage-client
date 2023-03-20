345bc336f071bd6af76076cea65677feadb5c6fe
benshabbat <benshabbat27@gmail.com>
2023-03-20 14:17:53 +0200

    added swiper slide

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import "./reviews.css";
 import CreateReviews from "../createReviews/CreateReviews";
 import { Rating } from "@mui/material";
+import { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/virtual";
+import "swiper/css/navigation";
 
 const Reviews = () => {
   const [openModel, setOpenModel] = useState(false);
   const handelClick = () => {
-      setOpenModel(!openModel);
-    };
-  
+    setOpenModel(!openModel);
+  };
+
   const reviewsCustomers = [
     {
       name: "tal",
@@ -36,24 +41,36 @@ const Reviews = () => {
     <div id="reviews" className="reviews">
       <h1>Reviews</h1>
       <div className="reviews-container">
-        {reviewsCustomers.map((customer, index) => {
-          return (
-            <div className="one-review" key={index}>
-              <h1>{customer.name}</h1>
-              <Rating
-                // style={{ fontSize: 45, top: "15px" }}
-                value={customer.stars}
-                readOnly
-              />
-              <div className="desc">{customer.desc}</div>
-            </div>
-          );
-        })}
+        <div className="reviews-slider">
+          <div className="slide-wrapper">
+            <Swiper
+              slidesPerView={3}
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {reviewsCustomers.map((customer, index) => {
+                return (
+                  <SwiperSlide className="one-review" key={index}>
+                    <h1>{customer.name}</h1>
+                    <Rating
+                      // style={{ fontSize: 45, top: "15px" }}
+                      value={customer.stars}
+                      readOnly
+                    />
+                    <div className="desc">{customer.desc}</div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+        </div>
       </div>
       <button onClick={handelClick}>Add Review</button>
-      {openModel && (
-        <CreateReviews handelClick={handelClick}/>
-      )}
+      {openModel && <CreateReviews handelClick={handelClick} />}
     </div>
   );
 };
