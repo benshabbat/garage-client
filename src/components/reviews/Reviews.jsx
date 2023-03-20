00d48f02f9df37f1f491d1b9546c596404c813b5
benshabbat <benshabbat27@gmail.com>
2023-03-20 15:32:19 +0200

    some ui on slide

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import "./reviews.css";
 import CreateReviews from "../createReviews/CreateReviews";
 import { Rating } from "@mui/material";
-import { Pagination, Navigation } from "swiper";
+import { FreeMode } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/virtual";
-import "swiper/css/navigation";
+import "swiper/css/free-mode";
+// import "swiper/css/navigation";
 
 const Reviews = () => {
   const [openModel, setOpenModel] = useState(false);
@@ -41,33 +41,46 @@ const Reviews = () => {
     <div id="reviews" className="reviews">
       <h1>Reviews</h1>
       <div className="reviews-container">
-        <div className="reviews-slider">
-          <div className="slide-wrapper">
-            <Swiper
-              slidesPerView={3}
-              spaceBetween={30}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-              className="mySwiper"
-            >
-              {reviewsCustomers.map((customer, index) => {
-                return (
-                  <SwiperSlide className="one-review" key={index}>
-                    <h1>{customer.name}</h1>
-                    <Rating
-                      // style={{ fontSize: 45, top: "15px" }}
-                      value={customer.stars}
-                      readOnly
-                    />
-                    <div className="desc">{customer.desc}</div>
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </div>
-        </div>
+        <Swiper
+          freeMode={true}
+          grabCursor={true}
+          modules={[FreeMode]}
+          className="mySwiper"
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween:10
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween:10
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween:15
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween:20
+            },
+          }}
+        >
+          {reviewsCustomers.map((customer, index) => {
+            return (
+              <SwiperSlide>
+                <div className="one-review" key={index}>
+                  <h1>{customer.name}</h1>
+                  <Rating
+                    // style={{ fontSize: 45, top: "15px" }}
+                    value={customer.stars}
+                    readOnly
+                  />
+                  <div className="desc">{customer.desc}</div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
       <button onClick={handelClick}>Add Review</button>
       {openModel && <CreateReviews handelClick={handelClick} />}
