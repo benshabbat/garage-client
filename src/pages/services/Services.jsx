fe0acf986bf67cdaf9ad43b5e6d5d0f5bc3f564a
benshabbat <benshabbat27@gmail.com>
2023-03-30 01:41:09 +0300

    user services in func

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getServicesByIdCar,getServicesByIdUser } from "../../features/user/userSlice";
+import {
+  getServicesByIdCar,
+  getServicesByIdUser,
+} from "../../features/user/userSlice";
 import { useParams } from "react-router-dom";
 import Service from "../../components/service/Service";
 
@@ -11,17 +14,28 @@ const Services = () => {
   useEffect(() => {
     if (carId) {
       dispatch(getServicesByIdCar(carId));
+    } else {
+      dispatch(getServicesByIdUser(user._id));
     }
-    dispatch(getServicesByIdUser(user._id))
   }, []);
 
   const carNumberPlate = user?.cars?.filter((car) => carId === car._id)[0]
     ?.numberPlate;
 
+  const servicesUser = services?.map((service) => {
+    return (
+      <div key={service._id}>
+        <h2>{`history Services for ${service.numberPlate} `}</h2>
+        <Service services={service.services} />
+      </div>
+    );
+  });
+
   return (
     <>
       {/* <h1>{`hello ${carId}`}</h1> */}
       <h2>{`history Services for ${carNumberPlate} `}</h2>
+      {console.log(services)}
       <Service services={services} />
     </>
   );
