3d77a3d0541f781aedfc0897b5113002ba82de32
benshabbat <benshabbat27@gmail.com>
2023-04-24 13:10:46 +0300

    navigate to "/" when click create user

--- src/components/NavAdmin.jsx
+++ src/components/NavAdmin.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import useOpenModel from "../hooks/useOpenModel";
 import Register from "../components/register/Register"
 const NavAdmin = () => {
   const { openModel, handelClick,setOpenModel } = useOpenModel();
   return (
     <>
+    <Link to="/">
        <div className="item-nav">
         <button onClick={handelClick}>Create User</button>
         <Register handelClick={handelClick} open={openModel} setOpen={setOpenModel} />
       </div>
+    </Link>
        {/* <div className="item-nav">
         <button onClick={handelClick}>Create Car</button>
         <Register handelClick={handelClick} open={openModel} setOpen={setOpenModel} />
