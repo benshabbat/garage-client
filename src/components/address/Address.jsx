0f5f21d05855b610c8f90a70d98c0a411aecfcfd
benshabbat <benshabbat27@gmail.com>
2023-04-27 18:45:02 +0300

    added map for address

--- src/components/address/Address.jsx
+++ src/components/address/Address.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import "./address.css"
-
-import AddressLink from "../addressLink/AddressLink"
+import "./address.css";
+import React from "react";
+import AddressLink from "../addressLink/AddressLink";
 const Address = () => {
   return (
-    <div id='address' style={{height:500}}><AddressLink address={"Jerusalem"}/></div>
-  )
-}
+    <div id="address">
+      <AddressLink address={"Lod"} />
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13542.273198574256!2d34.89606664947509!3d31.945475952361903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1siw!2sil!4v1682609954359!5m2!1siw!2sil"
+        width="600"
+        height="450"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
 
-export default Address
+export default Address;
