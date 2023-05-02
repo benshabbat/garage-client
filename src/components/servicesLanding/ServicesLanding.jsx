c285b6e989bc40c7f4ba2621fc9e678ce4ad653e
benshabbat <benshabbat27@gmail.com>
2023-05-02 22:20:57 +0300

    creeated service landing

--- src/components/servicesLanding/ServicesLanding.jsx
+++ src/components/servicesLanding/ServicesLanding.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import classes from "./ServicesLanding.module.css"
 
 const ServicesLanding = () => {
   return (
-    <div id="services" style={{ height: 500 }}>
-      ServicesLanding
+    <div className={classes.main} id="services" >
+      <h1>Our Services</h1>
+      <ul>
+        <li>Car Air Conditioning</li>
+       <li>Car Batteries</li>
+       <li>Car Brakes</li>
+       <li>Car Exhausts</li>
+       <li>Car Gearbox Repairs</li>
+       <li>Car Repairs</li>
+       <li>Car Servicing</li>
+       <li>Car Tyres</li>
+       <li>Car Welding</li>
+       <li>Classic Car Service</li>
+       <li>Clutch Replacement</li>
+       <li>Courtesy Car</li>
+       <li>Engine Diagnostics</li>
+       <li>Fleet Maintenance</li>
+     
+       <li>Mechanical Repairs</li>
+
+
+      </ul>
     </div>
   );
 };
