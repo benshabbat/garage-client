025cada2d612f208e899bd4ccbf037d466aeba2e
benshabbat <benshabbat27@gmail.com>
2023-05-09 12:16:28 +0300

    done format phone

--- src/components/edit/EditUser.jsx
+++ src/components/edit/EditUser.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import {Form,OpenModel} from "../index";
+import { Form, OpenModel } from "../index";
 import { updateUser } from "../../Utils";
-const EditUser = ({ handelClick, isOpen ,user }) => {
-
+const EditUser = ({ handelClick, isOpen, user }) => {
   const [formData, setFormData] = useState(user);
   const onSubmit = async (e) => {
     e.preventDefault();
-   await updateUser(user?._id,formData);
+    await updateUser(user?._id, formData);
     handelClick();
   };
 
@@ -19,10 +18,16 @@ const EditUser = ({ handelClick, isOpen ,user }) => {
           sec_title="enter your name & password"
           inputs={[
             // { name: "username", type: "text" },
-            { name: "username", type: "text" ,value:formData?.username},
-            { name: "email", type: "email" ,value:formData?.email},
-            { name: "phone", type: "phone" ,value:formData?.phone},
-            { name: "password", type: "password" ,value:formData?.password},
+            { name: "username", type: "text", value: formData?.username },
+            { name: "email", type: "email", value: formData?.email },
+            {
+              name: "phone",
+              type: "phone",
+              value: formData?.phone,
+              pattern: "[0-9]{3}[-][0-9]{7}|[0-9]{10}",
+              title: "Number of phone must 050-1234567",
+            },
+            { name: "password", type: "password", value: formData?.password },
           ]}
           handelClick={handelClick}
           onSubmit={onSubmit}
