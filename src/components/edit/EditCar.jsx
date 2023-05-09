79c5552d6c4150f923ace6ae1ceb03ec5c888836
benshabbat <benshabbat27@gmail.com>
2023-05-09 23:04:12 +0300

    valid for edit car

--- src/components/edit/EditCar.jsx
+++ src/components/edit/EditCar.jsx
@@ -17,8 +17,20 @@ const EditCar = ({ handelClick, isOpen, car }) => {
           title="Edit Car"
           sec_title="enter your name & password"
           inputs={[
-            { name: "numberPlate", type: "text", value: formData?.numberPlate },
-            { name: "km", type: "number", value: formData?.km ,min:formData?.km },
+            {
+              name: "numberPlate",
+              type: "text",
+              value: formData?.numberPlate,
+              pattern:
+                "[0-9]{3}[-][0-9]{2}[-][0-9]{3}|[0-9]{2}[-][0-9]{3}[-][0-9]{2}|[0-9]{7,8}",
+              title: "Number of car must 00-000-00 OR 000-00-000",
+            },
+            {
+              name: "km",
+              type: "number",
+              value: formData?.km,
+              min: formData?.km,
+            },
             { name: "brand", type: "text", value: formData?.brand },
           ]}
           handelClick={handelClick}
